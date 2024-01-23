a27b648af5c22a7a2954a017bc15f43ce3772873
kuldeep pandit <kuldeeppandit146@gmail.com>
2024-01-23 11:07:10 +0530

    domain layer useCase unit testing implemented

--- lib/features/number_checker/domain/usecases/get_random_concrete_number_checker_usecase.dart
+++ lib/features/number_checker/domain/usecases/get_random_concrete_number_checker_usecase.dart
@@ -5,23 +5,14 @@ import 'package:tdd_with_riverpod/core/usecases/usecase.dart';
 import 'package:tdd_with_riverpod/features/number_checker/domain/entities/number_checker.dart';
 import 'package:tdd_with_riverpod/features/number_checker/domain/repositories/number_checker_repository.dart';
 
-class GetConcreteNumberCheckerUseCase
-    implements UseCase<NumberCheckerEntity, Params> {
+class GetRandomConcreteNumberCheckerUseCase
+    implements UseCase<NumberCheckerEntity, NoParams> {
   final NumberCheckerRepository repository;
 
-  GetConcreteNumberCheckerUseCase(this.repository);
+  GetRandomConcreteNumberCheckerUseCase(this.repository);
 
   @override
-  Future<Either<Failure, NumberCheckerEntity>> call(Params params) async {
-    return await repository.getConcreteNumberChecker(params.number);
+  Future<Either<Failure, NumberCheckerEntity>> call(NoParams params) async {
+    return await repository.getRandomNumberChecker();
   }
-}
-
-class Params extends Equatable {
-  final int number;
-
-  const Params({required this.number});
-
-  @override
-  List<Object> get props => [number];
 }
--- test/features/number_checker/domain/usecases/get_random_concrete_number_checker_test.dart
+++ test/features/number_checker/domain/usecases/get_random_concrete_number_checker_test.dart
@@ -1,37 +1,38 @@
 import 'package:dartz/dartz.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tdd_with_riverpod/core/usecases/usecase.dart';
 import 'package:tdd_with_riverpod/features/number_checker/domain/entities/number_checker.dart';
 import 'package:tdd_with_riverpod/features/number_checker/domain/repositories/number_checker_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_with_riverpod/features/number_checker/domain/usecases/get_concrete_number_checker_usecase.dart';
+import 'package:tdd_with_riverpod/features/number_checker/domain/usecases/get_random_concrete_number_checker_usecase.dart';
 import 'get_concrete_number_checker_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<NumberCheckerRepository>()])
 void main() {
-  late GetConcreteNumberCheckerUseCase useCase;
+  late GetRandomConcreteNumberCheckerUseCase useCase;
   late MockNumberCheckerRepository mockNumberCheckerRepository;
 
   setUp(() {
     mockNumberCheckerRepository = MockNumberCheckerRepository();
-    useCase = GetConcreteNumberCheckerUseCase(mockNumberCheckerRepository);
+    useCase =
+        GetRandomConcreteNumberCheckerUseCase(mockNumberCheckerRepository);
   });
 
-  const tNumber = 1;
-  const tNumberResponse = NumberCheckerEntity(text: 'test', number: tNumber);
+  const tNumberResponse = NumberCheckerEntity(text: 'test', number: 1);
 
-  test('get concrete number from the repository ', () async {
+  test('get random concrete number from the repository ', () async {
     /// arrange
-    when(mockNumberCheckerRepository.getConcreteNumberChecker(tNumber))
+    when(mockNumberCheckerRepository.getRandomNumberChecker())
         .thenAnswer((_) async => const Right(tNumberResponse));
 
     /// act
-    final result = await useCase(const Params(number: tNumber));
+    final result = await useCase(NoParams());
 
     /// assert
     expect(result, const Right(tNumberResponse));
-    verify(mockNumberCheckerRepository.getConcreteNumberChecker(tNumber))
-        .called(1);
+    verify(mockNumberCheckerRepository.getRandomNumberChecker()).called(1);
     verifyNoMoreInteractions(mockNumberCheckerRepository);
   });
 }
